0690202a390a62ae302aafada81cb7d9bc384fa6
pranav <pranav8267@gmail.com>
2024-11-13 09:52:42 +0530

    music player

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,11 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
-import Signout from "./components/Signout";
-import Link from "next/link";
 
-async function Home() {
-  const session = await getServerSession(authOptions);
 
+const page = () => {
   return (
-    <>
-      <div>
-        <h1>Hi from Next.js Testing</h1>
-      </div>
-      <div>
-        {session?.user?.name ? (
-          <p>Welcome, {session.user.name}!</p>
-        ) : (
-          <p>You are not signed in.</p>
-        )}
-      </div>
-      {!session && (
-        <Link href="/auth/signin">
-          <button className="px-4 py-2 bg-blue-500 text-white rounded">
-            Sign In
-          </button>
-        </Link>
-      )}
-      {session && <Signout />}
-    </>
-  );
+    <div>
+      home page
+    </div>
+  )
 }
 
-export default Home;
+export default page
